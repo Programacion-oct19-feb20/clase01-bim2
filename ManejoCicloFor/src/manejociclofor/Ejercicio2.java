9589f74fe4a80ad18017e0c7d580d0971dba7146
@reroes <reroes799@gmail.com>
2019-12-05 10:06:28 -0500

    ejemplos ciclo for

---
+++ ManejoCicloFor/src/manejociclofor/Ejercicio2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejociclofor;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int limiteNumero = 10;
+        int limiteTabla = 10;
+        for (int i = 1; i <= limiteNumero; i++) {
+            System.out.printf("%d\n", i);
+            if(i==6){
+                break;
+            }
+        }
+        
+    }
+}
---
+++ ManejoCicloFor/src/manejociclofor/Ejericicio4.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejociclofor;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejericicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf("%d\n", i);
+            for (int j = 0; j <= 3; j++) {
+                System.out.printf("\t%d\n", j);
+            }
+        }
+        
+    }
+    
+}
